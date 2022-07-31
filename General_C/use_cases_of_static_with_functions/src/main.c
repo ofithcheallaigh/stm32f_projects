ef0c0afeb85ccecebe8d716327e8421872c4c642
ofithcheallaigh <sofithcheallaigh@gmail.com>
2022-07-31 18:34:49 +0100

    updating static keyword file

--- General_C/use_cases_of_static_with_functions/src/main.c
+++ General_C/use_cases_of_static_with_functions/src/main.c
@@ -16,3 +16,13 @@ int main()
 
 	return 0;
 }
+
+
+/*
+ * If we only want to be able to call this function from within the main.c file
+ * we need to use the 'static' keyword in front of the function
+ */
+static void change_system_clock(int system_clock)
+{
+	printf("System Clock changed to: %d\n", system_clock);
+}
