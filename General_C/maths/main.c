73dab310f5e5c92b8da0367e217f8dfa5f19c010
ofithcheallaigh <sofithcheallaigh@gmail.com>
2022-08-07 22:24:49 +0100

    adding new functions to math work

--- General_C/maths/main.c
+++ General_C/maths/main.c
@@ -3,7 +3,13 @@
 
 int main(void)
 {
-    printf("Add: %d\n", maths_add(0x0FFF1111, 0x0FFF1111));
+    printf("Add (decimal): %d\n", maths_add(0x0FFF1111, 0x0FFF1111));
+    printf("Add (hex): %X\n", maths_add(0x0FFF1111, 0x0FFF1111));
+
+    // To get the multiplication to work, we need to caste the data type in the functions
+    printf("Multiplication: %I64lli\n", maths_mul(0x0FFF1111, 0x0FFF1111)); // I64lli is the placeholder for a long long int
+
+    printf("Division: %0.2f\n", maths_div(100,8)); // The 0.2 sets the nuber of decimal places
 
     return 0;
 }
